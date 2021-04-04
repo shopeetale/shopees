6cd42e4062c2741ac3bb36271b2afd5721d03720
karthickuid <karthickuid@m2p.in>
2021-04-04 17:48:18 +0530

    2 contents added

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,16 @@
+$(document).ready(function () {
+    $(".sidenav").load("header.html");
+
+    setTimeout(function () {
+        $(".drop").click(function () {
+            $(this).parent().find(".drop-menu").slideToggle();
+        })
+    }, 300)
+
+
+})
+
+
 // for inn amazon
 jQuery.getJSON("../jsons/amazon.json", function (data) {
     console.log(data.length, "datas");
@@ -17,11 +30,54 @@ jQuery.getJSON("../jsons/amazon.json", function (data) {
         prolistteamp = prolistteamp + prohtml;
 
     }
-    console.log(prolistteamp);
-    $(".shop-cont-sec").append(prolistteamp);
+    $(".amazon").append(prolistteamp);
 
 });
 
-$(".drop").click(function () {
-    $(this).parent().find(".drop-menu").slideToggle();
-})
+// namecheap
+
+jQuery.getJSON("../jsons/namecheap.json", function (data) {
+    console.log(data.length, "nameheco");
+    var prolistteamp = "";
+
+    for (var i = 0; i < data.length; i++) {
+        var prohtml = `<div class="col-md-4">
+                                <div class="shp-dlt-sec">
+                                    <div class="pro-desc-bx">
+                                        <div class="shplogo"><img src="${data[i].logo}" alt="amazon" /></div>
+                                        <p class="shpcont">${data[i].content}</p>
+                                        <p class="pro-type">${data[i].product}</p>
+                                    </div>
+                                    <a href="${data[i].url}" class="btn btn-go">GET NOW</a>
+                                </div>
+                            </div>`;
+
+        prolistteamp = prolistteamp + prohtml;
+
+    }
+    $(".namecheap").append(prolistteamp);
+
+});
+
+// trip.com
+
+jQuery.getJSON("../jsons/trip.json", function (data) {
+    console.log(data.length, "nameheco");
+    var prolistteamp = "";
+
+    for (var i = 0; i < data.length; i++) {
+        var prohtml = `<div class="shp-dlt-sec">
+                                <div class="pro-desc-bx">
+                                    <div class="shplogo"><img src="${data[i].logo}" alt="amazon" /></div>
+                                    <p class="shpcont">${data[i].content}</p>
+                                    <p class="pro-type">${data[i].product}</p>
+                                </div>
+                                <a href="" class="btn btn-go">GET NOW</a>
+                            </div>`;
+
+        prolistteamp = prolistteamp + prohtml;
+
+    }
+    $(".trip").append(prolistteamp);
+
+});
