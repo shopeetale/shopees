19a059579c5b769b513bc30e9aaabcb39f175523
shopeetale <myshopeetale@gmail.com>
2021-05-03 23:12:07 +0530

    inc

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $(".sidenav").include("header.html");
+    $(".sidenav").load("header.html");
 
     setTimeout(function () {
         $(".drop").click(function () {
